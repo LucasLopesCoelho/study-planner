4c9d8d8cf685a49057edcf17cf1b62cfa392960e
LucasLopesCoelho <lucaslopescoelho09@gmail.com>
2026-09-14 00:12:50 -0300

    Create study_planner.py

---
+++ study_planner.py
@@ -0,0 +1,9 @@
+print("Welcome to Study Planner")
+
+subject = input("What subject do you want to study today? ")
+minutes = input("How many minutes do you want to study? ")
+
+print()
+print("Today's study plan:")
+print("Subject:", subject)
+print("Study time:", minutes, "minutes")
