19d5c9af5e95235ce9961234fdef1e327836fae1
LucasLopesCoelho <lucaslopescoelho09@gmail.com>
2026-09-14 00:45:54 -0300

    Update study_planner.py

--- study_planner.py
+++ study_planner.py
@@ -3,14 +3,16 @@ print("Welcome to Study Planner")
 subjects = []
 
 number_of_subjects = int(input("How many subjects do you want to study today? "))
-
+priority = int(input(f"Priority for {subject} (1 = low, 2 = medium, 3 = high): "))
 for i in range(number_of_subjects):
     subject = input(f"Enter subject {i + 1}: ")
     minutes = int(input(f"How many minutes for {subject}? "))
 
     subjects.append({
-        "subject": subject,
-        "minutes": minutes
+    "subject": subject,
+    "minutes": minutes,
+    "priority": priority
+})
     })
 
 print()
